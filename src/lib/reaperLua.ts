e632202b4ced3e298411a7dfbab88108b35148fc
coldestconcept4ever <coldestconcept@gmail.com>
2026-06-20 21:08:17 -0500

    font

--- src/lib/reaperLua.ts
+++ src/lib/reaperLua.ts
@@ -82,10 +82,10 @@ gfx.init("BEATGANGSTA • CONNECT", 1260, 1860)
 
 local function update_fonts()
   local s = state.scale or 1.0
-  gfx.setfont(1, "Outfit", math.floor(28 * s), 98) -- Title Font
-  gfx.setfont(2, "Outfit", math.floor(14 * s), 98) -- Button / Help Label Font
-  gfx.setfont(3, "Outfit", math.floor(18 * s), 98) -- Input Value Font
-  gfx.setfont(4, "Outfit", math.floor(12 * s), 98) -- Subtitle / Micro Font
+  gfx.setfont(1, "Inter", math.floor(28 * s), 98) -- Title Font
+  gfx.setfont(2, "Inter", math.floor(14 * s), 98) -- Button / Help Label Font
+  gfx.setfont(3, "Inter", math.floor(18 * s), 98) -- Input Value Font
+  gfx.setfont(4, "Inter", math.floor(12 * s), 98) -- Subtitle / Micro Font
 end
 
 update_fonts()
